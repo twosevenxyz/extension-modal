twosevenxyz/extension-modal

commit 333dd3598f56fc5bfe2fa68af5668fe0020fe240

video-entry: Added special header to signal that requests are from the modal

src/components/video-entry.vue
@@ -177,6 +177,11 @@ export default {
     if (this.entry.videoSelector === 'web') {
       const url = this.entry.videoURL
       const realUrl = this.url
+      const { entry: { headers } } = self
+      headers.push({
+        name: 'x-from-tab-modal',
+        value: 1
+      })
       if (url.startsWith('hls:')) {
         // This is a HLS video
         const config = {

commit fa813c238f5e4a14655bbcb1e48161eb006657cf

Fixed special header to be string
webRequest API was throwing error due to it expecting string and us
adding an integer. This bug was introduced in commit 333dd3598f56fc5bfe2fa68af5668fe0020fe240

src/components/video-entry.vue
@@ -279,7 +279,7 @@ export default {
       const { entry: { headers } } = self
       headers.push({
         name: 'x-from-tab-modal',
-        value: 1
+        value: '1'
       })
       if (url.startsWith('hls:')) {
         // This is a HLS video