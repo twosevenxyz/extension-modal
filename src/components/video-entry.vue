9738fb5d88466ed620fe46c3ca079c2cda8c195c
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2020-02-06 22:58:20 -0500

    video-entry: Align video to the center

--- src/components/video-entry.vue
+++ src/components/video-entry.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="card is-vcentered" :class="widthClass">
     <span class="close" @click="$emit('hide-entry', entry)"><md-close title="Hide video"/></span>
-    <div class="column is-one-quarter-tablet">
-      <div class="card-image">
+    <div class="column is-one-quarter-tablet is-flex">
+      <div class="card-image" style="align-self: center;">
         <video ref="plyrEl" />
       </div>
     </div>
