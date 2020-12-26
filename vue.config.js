e9d45bf821952a30e9550fb69d66dd306bf1ac76
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2020-12-26 01:35:09 -0500

    Minor dependency cleanup

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const webpack = require('webpack')
 
 function resolve (dir) {
   return path.join(__dirname, dir)
@@ -11,13 +10,8 @@ module.exports = {
     Object.assign(config.resolve.alias, {
       vue$: 'vue/dist/vue.runtime.js',
       '@': resolve('src'),
-      plyr: path.join(resolve('plyr'), 'src', 'js', 'plyr')
+      plyr: path.join(resolve('plyr'), 'dist', 'plyr.js')
     })
-    config.plugins.push(...[
-      new webpack.DefinePlugin({
-        MODE: `"${process.env.NODE_ENV === 'development' ? 'dev' : 'prod'}"`
-      })
-    ])
   },
   css: {
     loaderOptions: {
